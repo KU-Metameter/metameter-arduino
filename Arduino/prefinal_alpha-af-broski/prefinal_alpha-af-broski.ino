7add3d89dbe7b5782820fd568bf7bbafe4d5b21b
Logan Satterfield <logansatterfield@ku.edu>
2023-03-17 05:04:12 -0500

    prefinal compiles, needs physical testing

--- Arduino/prefinal_alpha-af-broski/prefinal_alpha-af-broski.ino
+++ Arduino/prefinal_alpha-af-broski/prefinal_alpha-af-broski.ino
@@ -3,22 +3,22 @@
 #include <SPI.h>
 #include <Wire.h>
 
-
+//these are the pin assignments of the first revision of the multimeter schematic PRONE TO CHANGE 
 #define BUTTON 4 //on board button, use as necessary
 #define V_ADC A0 //voltage and ohm adc positive input
 #define COM_ADC A1 //common (negative) adc input
 #define OHM_OUT 16 //ohmeter voltage divider supply output 
 #define mA_ADC A3 //mA adc positive input
 #define A_ADC A4 //A adc positive input
-#define TFT_CS D2 //display chip select pin, used in setup call
+#define TFT_CS 2 //display chip select pin, used in setup call
 #define TFT_DC  21 //display disconnect pin, used in setup call
 #define TS_CS  22 //touch screen chip select pin, used in setup call
-#define TS_IRQ D7 //touch screen interrupt pin, used in setup or standalone (default high)
-#define V_OHM_CTL D9 //V/Ohm selector, high for voltmeter
-#define UH_CTL D10 //control pin for UH range (1k)
-#define H_CTL D11 //control pin for High range (9k)
-#define M_CTL D12 //control pin for Medium range (90k)
-#define L_CTL D13 //control pin for Low range (900k) (!!Exposes ADC to raw V/ohm In!!) 
+#define TS_IRQ 7 //touch screen interrupt pin, used in setup or standalone (default high)
+#define V_OHM_CTL 9 //V/Ohm selector, high for voltmeter
+#define UH_CTL 10 //control pin for UH range (1k)
+#define H_CTL 11 //control pin for High range (9k)
+#define M_CTL 12 //control pin for Medium range (90k)
+#define L_CTL 13 //control pin for Low range (900k) (!!Exposes ADC to raw V/ohm In!!) 
 
 //XPT2046_Touchscreen ts(TS_CS);
 XPT2046_Touchscreen ts(TS_CS, TS_IRQ);
@@ -56,8 +56,15 @@ void printMenu(){
   
 }
 
-void simpPrint3(){
-
+void simpPrint3(char* toPrint, float toPrint2, float toPrint3){
+  tft.fillRect(0, 0, 240, v_off-h_off, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(0,0);
+  tft.print(toPrint);
+  tft.setCursor(0,20);
+  tft.print(toPrint2);
+  tft.setCursor(0,40);
+  tft.print(toPrint3);
 }
 
 //manually calibrated touch screen edges, not 100% accurate
@@ -91,14 +98,14 @@ bool touchInBoxPoint(int x, int y, int w, int h, TS_Point point){
 
 
 
-const float adc2v = 3.3/3745; // manually calibrated, only works for 12 bit
-const int lowRangeRes = 900000;
+const float adc2v = 3.3/3745; // manually calibrated, only works for 12 bit //used for all data input functions //different for each itsybitsy?
+const int lowRangeRes = 900000; // values also used for ohmmeter 
 const int medRangeRes = 90000;
 const int highRangeRes = 9000;
 const int uhRangeRes = 1000;
 const int totRes = lowRangeRes + medRangeRes + highRangeRes + uhRangeRes;
-//copied from voltmeter sketch, needs to be updated with new variable names, probably still gets fucky with negative voltages
-//could be more accurate if parallel resistance of adc was accounted for
+//copied from voltmeter sketch,  probably still gets fucky with negative voltages
+//could be more accurate if parallel resistance of ADC was accounted for
 //could be more accurate if multiple samples were taken and averaged
 void voltMeter(){
   int read = 0;
@@ -135,48 +142,76 @@ void voltMeter(){
   
 }
 
+
+//used voltage divider equation solved for R2 from https://en.wikipedia.org/wiki/Voltage_divider#Resistive_divider
+//could probably add a fudge factor for series resistance of 3 relays and probes, maybe just subtract from final value?
 void ohmMeter(){
   int read = 0;
-  digitalWrite(H_CTL, HIGH);
-  digitalWrite(V_OHM_CTL, LOW);
-  digitalWrite(OHM_OUT, HIGH);
+  digitalWrite(L_CTL, HIGH); // connect v/ohm in to adc for all of ohmeter reading
+  digitalWrite(V_OHM_CTL, LOW); //disconnect bot of r ladder from in common
+  digitalWrite(OHM_OUT, HIGH); //ohm v source
+  digitalWrite(M_CTL, LOW); //setting defaults for robustness
+  digitalWrite(H_CTL, LOW);
+  digitalWrite(UH_CTL, LOW);
   delay(2); //required for relay switching 
-  read = analogRead(V_ADC) - (analogRead(COM_ADC));
-  if(abs(read) < 400){
-    digitalWrite(H_CTL, LOW);
-    digitalWrite(M_CTL, HIGH);
+  read = analogRead(V_ADC) - (analogRead(COM_ADC)); //read with full resistor ladder (1M)
+  if(read < 400){
+    //digitalWrite(, LOW);
+    digitalWrite(M_CTL, HIGH); //bypass 900k
     delay(2); //relay switch
-    read = analogRead(V_ADC) - (analogRead(COM_ADC));
-    if(abs(read) < 400){
-      digitalWrite(M_CTL, LOW);
-      digitalWrite(L_CTL, HIGH);
+    read = analogRead(V_ADC) - (analogRead(COM_ADC)); //read with all but 900k (100k)
+    if(read < 400){
+      //digitalWrite(, LOW);
+      digitalWrite(H_CTL, HIGH); //bypass 90k
       delay(2); //relay switch
-      read = analogRead(V_ADC) - (analogRead(COM_ADC));
-      simpPrint3("Low Range",read*adc2v*totRes/(lowRangeRes + medRangeRes + highRangeRes + uhRangeRes), read);
-      digitalWrite(L_CTL, LOW);
-      delay(2); //relay switch
+      read = analogRead(V_ADC) - (analogRead(COM_ADC)); //read with 9k and 1k (10k)
+      if(read < 400){
+        //digitalWrite(, LOW);
+        digitalWrite(UH_CTL, HIGH); //bypass 9k
+        delay(2);
+        read =  analogRead(V_ADC) - (analogRead(COM_ADC)); //read with 1k //TODO take into account relay resistance (~10 ohms)
+        simpPrint3("1k range",((1/((3.3/(read*adc2v))-1))*(uhRangeRes)), read);
+        //digitalWrite(UH_CTL, LOW);
+        //delay(2); //relay switch
+      }
+      else{
+        simpPrint3("10k range",((1/((3.3/(read*adc2v))-1))*(uhRangeRes + highRangeRes)), read);
+        //digitalWrite(, LOW);
+        //delay(2); //relay switch
+      }
     }
     else{
-      simpPrint3("Med Range",read*adc2v*totRes/(medRangeRes + highRangeRes + uhRangeRes), read);
-      digitalWrite(M_CTL, LOW);
-      delay(2); //relay switch
+      simpPrint3("100k range",((1/((3.3/(read*adc2v))-1))*(uhRangeRes + highRangeRes + medRangeRes)), read);
+      //digitalWrite(, LOW);
+      //delay(2); //relay switch
     }
   }
   else{
-    simpPrint3("High Range",read*adc2v*totRes/(highRangeRes + uhRangeRes), read);
-    digitalWrite(H_CTL, LOW);
-    digitalWrite(V_OHM_CTL, HIGH);  //may need to change, refer to call in setup
-    digitalWrite(OHM_OUT, LOW);
-    delay(2); //relay switch
+    simpPrint3("1M range",((1/((3.3/(read*adc2v))-1))*(uhRangeRes + highRangeRes + medRangeRes + lowRangeRes)), read);
   }    
-}
-
-void ampMeter(){
-
-}
-
-void miliampMeter(){
   
+  digitalWrite(L_CTL, LOW); //refer to beginning of function 
+  digitalWrite(M_CTL, LOW); //resetting to defaults
+  digitalWrite(H_CTL, LOW);
+  digitalWrite(UH_CTL, LOW);  
+  digitalWrite(V_OHM_CTL, HIGH);  //may need to change, refer to call in setup
+  digitalWrite(OHM_OUT, LOW);
+  delay(2); //relay switch
+}
+
+
+const float ampRes = 0.05; 
+void ampMeter(){
+  int read = 0;
+  read = analogRead(A_ADC) - (analogRead(COM_ADC));
+  simpPrint3("10A Range", read*adc2v/ampRes, read);  //ohms law, bitch
+}
+
+const int miliampRes = 5;
+void miliampMeter(){
+  int read = 0;
+  read = analogRead(A_ADC) - (analogRead(COM_ADC));
+  simpPrint3("10A Range", read*adc2v/miliampRes, read);  //ohms law, bitch part 2
 }
 
 void setup() {
@@ -215,52 +250,52 @@ int curTime = 0;
 int lastTime = 0;
 int mode = 0;  //
 void loop() {  //utilizing zoomy loops to avoid using interrupts (double triggering, never felt responsive enough) keep everything in the main loop fast (or you die)
-  curTime = milis();
+  curTime = millis();
   if(curTime >= lastTime + 500){  //run this section of the loop every half second 
     if(mode == 1){
-      //BTmenu();
+      //BTmenu(); //ANDREW DO YOUR F***ING JOB >:( (i love you)
     }
     else if(mode == 2){
-      //diodeMeter();
+      //diodeMeter(); //TODO How the fuck do diodes work
     }
     else if(mode == 3){
-      miliampMeter();
+      miliampMeter(); //takes approx 0 ms, no relays
     }
     else if(mode == 4){
       voltMeter();  //take approx 4 to 8 ms, could possibly be improved with better relay characterization
     }
     else if(mode == 5){
-      ohmMeter();
+      ohmMeter(); //takes approx 4 to 10 ms, see voltmeter call
     }
     else if(mode == 6){
-      ampMeter();
+      ampMeter(); //takes approx 0 ms, no relay switching
     }
 
 
     
-    lastTime = milis(); //run at end of loop for funsies
+    lastTime = millis(); //run at end of loop for funsies
   }
 
   if (ts.tirqTouched()) { //check for touch, probably needs to be run rapidly to feel responsive  //TODO: Check how long it takes to run
-    if (ts.touched()) {  //not sure if this nest is necessary 
-      TS_Point p = ts.getPoint();
+    if (ts.touched()) {  //not sure if this nest is necessary (nestecarry ehehuhehuhe)
+      TS_Point point = ts.getPoint();
       
-      if(touchInBox(h_off*0, v_off, h_off, h_off, point)){
+      if(touchInBoxPoint(h_off*0, v_off, h_off, h_off, point)){
         mode = 1;
       }
-      else if(touchInBox(h_off*1, v_off, h_off, h_off, point)){
+      else if(touchInBoxPoint(h_off*1, v_off, h_off, h_off, point)){
         mode = 2;
       }
-      else if(touchInBox(h_off*2, v_off, h_off, h_off, point)){
+      else if(touchInBoxPoint(h_off*2, v_off, h_off, h_off, point)){
         mode = 3;
       }
-      else if(touchInBox(h_off*0, v_off-h_off, h_off, h_off, point)){
+      else if(touchInBoxPoint(h_off*0, v_off-h_off, h_off, h_off, point)){
         mode = 4;
       }
-      else if(touchInBox(h_off*1, v_off-h_off, h_off, h_off, point)){
+      else if(touchInBoxPoint(h_off*1, v_off-h_off, h_off, h_off, point)){
         mode = 5;
       }
-      else if(touchInBox(h_off*2, v_off-h_off, h_off, h_off, point)){
+      else if(touchInBoxPoint(h_off*2, v_off-h_off, h_off, h_off, point)){
         mode = 6;
       }
     }
