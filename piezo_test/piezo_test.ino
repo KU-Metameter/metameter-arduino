bf91ad9dad971df0c4532dbbde885f7f01af2f71
Logan Satterfield <logansatterfield@ku.edu>
2023-03-22 10:00:37 -0500

    began work with piezo speaker

---
+++ piezo_test/piezo_test.ino
@@ -0,0 +1,35 @@
+
+
+#define _PWM_LOGLEVEL_       4
+#define USING_TIMER       false   //true
+
+#include "nRF52_PWM.h"
+#define pinToUse       13
+
+//creates pwm instance
+nRF52_PWM* PWM_Instance;
+
+float frequency = 1000.0f;
+
+float dutyCycle = 0.0f;
+
+void setup()
+{
+  PWM_Instance = new nRF52_PWM(pinToUse, frequency, dutyCycle);
+}
+
+void playFreq(float freq, float sec){
+  frequency = freq;
+  dutyCycle = 50.0f;
+  PWM_Instance->setPWM(pinToUse, frequency, dutyCycle);
+  delay(sec);
+}
+
+
+
+void loop()
+{
+  for(int i = 3000; i < 5000; i++){
+    playFreq(i,5);
+  }
+}
